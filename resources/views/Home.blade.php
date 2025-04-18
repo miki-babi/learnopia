82844ea524b2f936f804ddad7b79b766bc2f8c70
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-19 02:17:06 +0300

    set up

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telegram Mini App</title>
+    {{-- <script src="https://telegram.org/js/telegram-web-app.js"></script> --}}
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body x-data= "{
+     open: false,
+}" class="bg-white" id="app">
+    <div class="text-sky-500 text-2xl font-bold p-4 rounded-lg bg-gray-100 shadow-md flex items-center justify-between">
+        <div class="text-black"  @click="open = !open">
+            <div x-show="!open" class="p-2 hover:bg-gray-200  border-2 rounded-md cursor-pointer">
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+            </div>
+            <div x-show="open" class="p-2 hover:bg-gray-200  border-2 rounded-md cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>   
+            </div>
+        </div>
+        <div class="flex flex-row items-center">
+            <p class="mr-4">
+                {{ Auth::user()->name }}  
+            </p> 
+            <img src="{{ $photoUrl }}" alt="User Photo" class="rounded-full w-16 h-16">
+        </div>
+    </div>
+    <div  x-show="open"  class ="absolute top-16 left-4 bg-white shadow-lg rounded-lg p-4 z-10">
+        
+        <ul>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
+                <a href="#">Dashboard</a>
+            </li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
+                <a href="#">Logout</a>
+            </li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
+                <a href="#">Profile</a>
+            </li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
+                <a href="#">Settings</a>
+            </li>
+            <li class="p-2 hover:bg-gray-200 cursor-pointer" @click="open = !open">
+                <a href="#">Help</a>
+            </li>
+        </ul>
+    </div>
+    <h1   class="text-2xl">
+        {{-- {{ dd(Auth::user()) }} --}}
+       welcome   {{ Auth::user()->name }}  
+    </h1>
+
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function (Request $request) {
+    // Extract user data from request
+    $telegramId = $request->input('user_id');
+    $firstName = $request->input('first_name');
+    $lastName = $request->input('last_name');
+    $username = $request->input('username');
+    $photoUrl = $request->input('photo_url');
+
+    // Check if user exists
+    $user = User::where('telegram_id', $telegramId)->first();
+
+    if (!$user) {
+        // Create new user if not found
+        $user = User::create([
+            'name' => $firstName . ' ' . $lastName,
+            'telegram_id' => $telegramId,
+            'phone' => null, // Phone is nullable
+        ]);
+    }
+
+    // Authenticate user
+    Auth::loginUsingId($user->id);
+
+    // Store user in session
+    Session::put('user', $user);
+
+    return view('dashboard', compact(['user', 'firstName', 'lastName', 'username', 'photoUrl']));
+})->name('dashboard');
